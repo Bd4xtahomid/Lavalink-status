142ec4119bc6540ce68f92ba1b5c19d4f3785fdb
Tahomid op <ffconfig.op@gmail.com>
2025-04-03 12:06:52 +0600

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   token: process.env.token || "", // Ensure this env variable is set
-  channelId: process.env.channelId || "",
+  channelId: process.env.channelId || "1355561533014544414",
 
   webMonitor: true, // Set to false if you don't want a website
   expressPort: process.env.expressPort || 3000,
@@ -8,9 +8,9 @@ module.exports = {
   nodes: [
     {
       host: "78.46.65.243", // Your lavalink host address 
-      password: "youshallnotpass", // Your lavalink password
+      password: "dsc.gg/Zahard23", // Your lavalink password
       port: 4626, // Your lavalink port
-      identifier: "Node", // Name for your lavalink
+      identifier: "Main Node V4", // Name for your lavalink
       secure: false, // set to true if your lavalink has SSL
       reconnectTimeout: 300000,
       reconnectTries: 100,
